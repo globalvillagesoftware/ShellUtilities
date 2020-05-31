8c57627d46eb5274c6e2a45ea58949f5cf1f75fc
Jgossage <jgossage@gmail.com>
2020-05-31 18:57:16 -0400

    The global village Python virtual environment setup has changed

--- src/activategv.sh
+++ src/activategv.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # This module will activate the Python virtual environment in a Ubuntu shell for
-cd /LinuxData/jgossage/venvs/3.6.10/GlobalVillage/bin
+cd /LinuxData/jgossage/venvs/3.8.2/GlobalVillage/bin
 source activate
