22e7104c55aed04035026dbe83ce15e16080ee0f
Jgossage <jgossage@gmail.com>
2020-05-31 19:02:59 -0400

    This script is obsolete
    Because we found how to add Eclipse to the task bar.

--- src/activategv.sh
+++ src/activategv.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
-# This module will activate the Python virtual environment in a Ubuntu shell for
-cd /LinuxData/jgossage/venvs/3.8.2/GlobalVillage/bin
+# This module will activate the Python virtual environment in a Ubuntu shell for the Global Village
+# using Python 3.8.3
+cd /LinuxData/jgossage/venvs/GlobalVillage/3.8.3/bin
 source activate
