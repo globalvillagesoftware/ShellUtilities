3ac7f1aa336e5ffe030a40c6974c3acc4162ca5a
Jgossage <jgossage@gmail.com>
2020-06-22 00:13:54 -0400

    Work in progress

--- src/activategv.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-# This module will activate the Python virtual environment in a Ubuntu shell for the Global Village
-# using Python 3.8.3
-cd /LinuxData/jgossage/venvs/GlobalVillage/3.8.3/bin
-source activate
